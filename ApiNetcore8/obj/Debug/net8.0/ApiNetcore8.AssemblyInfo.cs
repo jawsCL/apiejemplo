a8897d77e88fb5e116ada87d0fbd2083fd53455c
jawsCL <gonzalo.alvarado@outlook.com>
2024-06-04 10:03:45 -0400

    Actualizacion 0506

--- ApiNetcore8/obj/Debug/net8.0/ApiNetcore8.AssemblyInfo.cs
+++ ApiNetcore8/obj/Debug/net8.0/ApiNetcore8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiNetcore8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a507facdd614abd5ef70d03aef30ca1e6e4516")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiNetcore8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiNetcore8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
